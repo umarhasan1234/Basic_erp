7a291b67f90b98de3fef7bc96c0939706b9cf860
VinodYogi6263 <rajguruv737@gmail.com>
2023-08-17 03:06:04 -0700

    first commit

--- src/main/java/com/nrt/controller/CouponController.java
+++ src/main/java/com/nrt/controller/CouponController.java
@@ -2,16 +2,12 @@ package com.nrt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
-import com.nrt.entity.Coupon;
 import com.nrt.request.CouponRequest;
 import com.nrt.service.CouponService;
 
